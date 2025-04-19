50dcce80d21dea0935631975b1987286a80afe99
CarmenGil05 <gilvazquezcarmendelrocio@gmail.com>
2025-04-19 03:48:46 +0200

    upt4

--- src/clase1.java
+++ src/clase1.java
@@ -8,6 +8,7 @@ public class clase1 {
 		System.out.println("Hola, esta es la version 1.20");
 		System.out.println("Hola, esta es la version 1.30");
 		System.out.println("Hola, esta es la version 1.40");
+		System.out.println("Hola, esta es la version 1.50");
 
 		
 	}
